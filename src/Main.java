6b1235286767e544434ceeb0bf8f057b931f17bb
innabilous <inna.bilous@broadcom.com>
2020-01-15 23:37:03 +0100

    Third edit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Play Method");
         System.out.println("Play Method2");
+        System.out.println("Play Method3");
 
     }
 }
